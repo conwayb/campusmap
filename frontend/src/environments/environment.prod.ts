fb555f5a09195db71e27455020fcebe2d888aa8d
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-05-15 16:17:35 -0700

    Add production Bing maps key

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-    production: true
+    production: true,
+    map: {
+        bing: {
+            key: 'AgxmzstikBLmO_mV3r12gvD3JD7Wc2B4ItlHpL-yaZK-EtnREzEEGMDJLr-t9VMy'
+        }
+    }
 };
