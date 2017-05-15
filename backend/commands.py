997081977b9e6a15e9e24f9a72f715f81d867426
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-05-15 15:54:09 -0700

    Add initial import_geojson command (buildings only)

--- backend/commands.py
+++ backend/commands.py
@@ -1,6 +1,9 @@
+import os
+
 from runcommands import command
 
 from arctasks.commands import *
+from arctasks.django import setup as django_setup
 
 
 @command(env='dev', timed=True)
@@ -9,3 +12,14 @@ def init(config, overwrite=False, drop_db=False):
     install(config)
     createdb(config, drop=drop_db)
     migrate(config)
+
+
+@command(default_env='dev', timed=True)
+def import_geojson(config, source_dir='../gisdata', overwrite=False, verbose=False, quiet=False,
+                   dry_run=False):
+    django_setup(config)
+    from campusmap.buildings.importer import BuildingsImporter
+
+    importer = BuildingsImporter(
+        source_dir, overwrite=overwrite, verbose=verbose, quiet=quiet, dry_run=dry_run)
+    importer.run()
