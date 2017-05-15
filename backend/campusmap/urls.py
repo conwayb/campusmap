02e651fa1ce488a2e0a8ebf88bd31cb1a305de6e
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-05-15 16:00:49 -0700

    Wire up Django Admin

--- backend/campusmap/urls.py
+++ backend/campusmap/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
+from django.http import HttpResponse
+
+from arcutils import admin
 from arcutils.cas import urls as cas_urls
 
 from . import views
 
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponse('Nothing to see here'), name='home'),
     url(r'^app-config$', views.AppConfigView.as_view(), name='app-config'),
+    url(r'^admin/', include(admin.cas_site.urls)),
     url(r'^account/', include(cas_urls)),
 ]
