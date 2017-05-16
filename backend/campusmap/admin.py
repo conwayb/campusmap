1cbaf27beebecb4c829f8d8697295c4d6a39f4f7
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-05-16 11:48:14 -0700

    Use the OpenStreetMap GIS admin class
    This replaces the default GIS admin class that shows a blurry topo map.

--- backend/campusmap/admin.py
+++ backend/campusmap/admin.py
@@ -1,6 +1,8 @@
+from django.contrib.gis.admin import OSMGeoAdmin
+
 from arcutils.admin import cas_site as site
 
 from .buildings.models import Building
 
 
-site.register(Building)
+site.register(Building, OSMGeoAdmin)
