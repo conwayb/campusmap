d59c98878610886ec0588c5fba162601fba57ec8
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-05-17 12:00:57 -0700

    Add hex and RGB JS constants

---
+++ frontend/src/constants.ts
@@ -0,0 +1,37 @@
+export const colorsHex = {
+    psuRed: '#d2492a',
+    psuGreen: '#8c9516',
+    psuBlue: '#00759a',
+
+    psuBrown: '#60351d',
+    psuSienna: '#a33f1f',
+    psuOrange: '#dc9b32',
+    psuYellow: '#e6dc8f',
+    psuTan: '#e8d3a2',
+
+    psuAccentGreen: '#a8b400',
+
+    psuPurple: '#650360',
+    psuLightBlue: '#dff2f5',
+
+    psuGray: '#474334'
+};
+
+export const colorsRGB = {
+    psuRed: [210, 73, 42],
+    psuGreen: [106, 127, 17],
+    psuBlue: [0, 117, 154],
+
+    psuBrown: [96, 53, 29],
+    psuSienna: [163, 63, 31],
+    psuOrange: [220, 155, 50],
+    psuYellow: [230, 220, 143],
+    psuTan: [232, 211, 162],
+
+    psuAccentGreen: [168, 180, 0],
+
+    psuPurple: [101, 3 ,96],
+    psuLightBlue: [161, 216, 224],
+
+    psuGray: [71, 67, 52]
+};
