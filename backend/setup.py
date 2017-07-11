90ac50086577884a0e3e5fc1402d912da6a49177
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-07-11 11:48:47 -0700

    Upgrade all Python packages

--- backend/setup.py
+++ backend/setup.py
@@ -12,19 +12,19 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django>=1.11',
-        'django-arcutils>=2.18.0',
-        'django-local-settings>=1.0b6',
+        'django>=1.11.3',
+        'django-arcutils>=2.22.0',
+        'django-local-settings>=1.0b7',
         'django-pgcli>=0.0.2',
-        'djangorestframework>=3.6.2',
+        'djangorestframework>=3.6.3',
         'Markdown>=2.6.8',
-        'psycopg2>=.2.7.1',
+        'psycopg2>=2.7.1',
         'psu.oit.arc.tasks',
     ],
     extras_require={
         'dev': [
             'coverage',
-            'django-cors-headers>=2.0.2',
+            'django-cors-headers>=2.1.0',
             'flake8',
             'gunicorn',
         ]
