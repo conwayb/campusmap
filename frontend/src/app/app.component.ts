dd2f9b8bb4c21fbcfec4c69868652f64f03bcdf5
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-07-12 09:26:02 -0700

    Position app & all top level components absolutely

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'psu-campusmap-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     name = 'PSU Campus Map App';
